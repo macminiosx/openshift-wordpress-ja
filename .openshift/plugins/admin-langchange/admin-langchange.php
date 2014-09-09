ed405e97865bb75a34e4f93117313c19256b24c0
Fukui Saeko <cloud@saeko.us>
2014-09-09 14:27:10 +0900

    plugin add

---
+++ .openshift/plugins/admin-langchange/admin-langchange.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: admin lang change
+Plugin URI: https://github.com/macminiosx/openshift-wordpress-ja/tree/master/.openshift/plugins
+Description:  Change lang admin in 'ja' and site in 'en' plugin.
+
+Version: 0.1
+Author: macminiosx
+Author URI: http://www.macminiosx.com/
+
+*/
+
+add_filter('locale', 'Admin_lang_change_func');
+
+function Admin_lang_change_func() {
+	if (strpos($_SERVER['REQUEST_URI'], '/wp-admin/') === 0) {
+		return 'ja';
+	} else {
+		return 'en';
+	}
+
+}
+
